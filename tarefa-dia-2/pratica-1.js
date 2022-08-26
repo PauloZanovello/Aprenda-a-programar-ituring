1dcfd31fdf57f9ffe9573b2a4bac27c25a03841f
PauloZanovello <zanovello.paulo@gmail.com>
2022-08-26 15:08:22 -0300

    pratica-1.js agencia de viagens

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,3 +10,14 @@ let profissao = "Líder de produção"
 let idade = 38
 let viagem = "Parque/Museu INHOTIM"
 
+console.log (" ")
+console.log (" ----------------- ")
+
+console.log (nome, email, profissao, idade, viagem)
+
+console.log ("  ")
+console.log (" ---------------- ")
+
+let mensagem = "Olá, " + nome + " ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagem + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log (mensagem)
