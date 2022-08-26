fbf94bd0a3c7f7dc9e285ee4802a114cb80685fc
PauloZanovello <zanovello.paulo@gmail.com>
2022-08-26 14:53:24 -0300

    escrevendo pratica.1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+// console.log ("Paulo R. Zanovello Jr.")
+// console.log ("zanovello.paulo@gmail.com")
+// console.log ("Líder de produção")
+// console.log ("38 anos")
+// console.log ("Parque/Museu Inhotim")
+
+let nome = "Paulo Zanovello Jr"
+let email ="zanovello.paulo@gmail.com"
+let profissao = "Líder de produção"
+let idade = 38
+let viagem = "Parque/Museu INHOTIM"
+
