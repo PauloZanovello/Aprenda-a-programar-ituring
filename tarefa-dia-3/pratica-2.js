0d13e98a68bcfa898d261587c79fb602f7c950cb
PauloZanovello <zanovello.paulo@gmail.com>
2022-08-29 14:46:36 -0300

    tabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,9 @@
+const multiplicador = 5
+let i = 1
+
+while (i <= 10) {
+    const resultado = i*multiplicador
+    const valorexibido = `${multiplicador} X ${i} = ${resultado}`
+    i++
+    console.log (valorexibido)
+}
